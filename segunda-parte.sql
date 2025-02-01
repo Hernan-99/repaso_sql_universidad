825bccf0c19eb57299fd86a1eb66150242b3c151
Hernan-99 <hernan24744@gmail.com>
2025-02-01 17:16:07 -0300

    Resolucion de ejercicios 5, 6, 7, 8 y 9

--- segunda-parte.sql
+++ segunda-parte.sql
@@ -12,4 +12,20 @@ SELECT SUM(length) AS duracion_para_pg FROM film WHERE rating='PG';
 SELECT SUM(length) AS duracion_para_pg FROM film WHERE rating='PG' AND title LIKE 'A%';
 
 --- 4
-SELECT AVG(rating) FROM film;
+SELECT AVG(rating) AS promedio_calificacion FROM film;
+
+--- 5
+SELECT AVG(rating) AS cal_prom_terminacion_S FROM film WHERE title LIKE '%s';
+
+--- 6
+SELECT MIN(length) AS calificacion_minima, MAX(length) AS calificacion_maxima FROM film;
+SELECT MIN(length) AS calificacion_minima, MAX(length) AS calificacion_maxima FROM film WHERE title LIKE 'E%';
+
+--- 7
+SELECT release_year, COUNT(*) AS año_estreno FROM film GROUP BY release_year;
+
+--- 8
+SELECT release_year, AVG(length) AS d_promedio, MIN(length) AS d_min, MAX(length) AS d_max FROM film GROUP BY release_year;
+
+--- 9 
+SELECT title, AVG(length) AS d_promedio, MIN(length) AS d_min, MAX(length) AS d_max FROM film GROUP BY title HAVING MIN(length) > 90;
