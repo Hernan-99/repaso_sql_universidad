8bed22e8cc8177d600543f3244b18e30ee4a440c
Hernan-99 <hernan24744@gmail.com>
2025-01-31 21:56:14 -0300

    resolucion de ejercicios 5, 6 y 7

--- primer_parte.sql
+++ primer_parte.sql
@@ -31,7 +31,25 @@ SELECT title AS pelicula, length AS duracion FROM film WHERE length >=60;
 
 -- EJERCICIO 4
 SELECT customer_id, first_name, active FROM customer WHERE customer_id > 50 AND active > 0;
+-------------------------------------------------------
+-------------------------------------------------------
+-------------------------------------------------------
 
+-- EJERCICIO 5
+SELECT customer_id, first_name, last_name FROM customer WHERE customer_id > 50 OR active >= 0;
+
+-------------------------------------------------------
+-------------------------------------------------------
+-------------------------------------------------------
+
+-- EJERCICIO 6
+SELECT actor_id, first_name, last_name FROM actor WHERE first_name IN ('PENELOPE', 'NICK', 'ED');
+-------------------------------------------------------
+-------------------------------------------------------
+-------------------------------------------------------
+
+-- EJERCICIO 7
+SELECT title, rating, length FROM film WHERE rating IN ('PG', 'PG-13', 'G') AND length BETWEEN  90 AND 120;
 
 
 -- ejercicio 1
